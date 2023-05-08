0f0cd382c66623ba9c837907adc0404ddff85712
saurabh4123 <saurabhdixit412@gmail.com>
2023-05-08 13:16:59 +0530

    Changes done

--- src/pages/TeacherDashboard.jsx
+++ src/pages/TeacherDashboard.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React,{useState} from 'react'
+import '../css/panels.css'
 
 function TeacherDashboard() {
+  const [activePanel, setActivePanel] = useState("panel1");
+
+  const handleClick = (panel) => {
+    setActivePanel(panel);
+  };
+
   return (
-    <div>TeacherDashboard</div>
-  )
+    <div className="two-panel-layout">
+      <div className="panel-switcher">
+        <button
+          className={`panel-button ${activePanel === "panel1" ? "active" : ""}`}
+          onClick={() => handleClick("panel1")}
+        >
+          Your Profile
+        </button>
+        <button
+          className={`panel-button ${activePanel === "panel2" ? "active" : ""}`}
+          onClick={() => handleClick("panel2")}
+        >
+          Your activities
+        </button>
+      </div>
+      <div className={`panel ${activePanel === "panel1" ? "active" : ""}`}>
+        <h2>Panel 1 Content</h2>
+        <p>
+          Lodu
+        </p>
+      </div>
+      <div className={`panel ${activePanel === "panel2" ? "active" : ""}`}>
+        <h2>Panel 2 Content</h2>
+        <p>
+          Lalit
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default TeacherDashboard
