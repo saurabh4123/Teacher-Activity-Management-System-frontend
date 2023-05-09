eb9cb18809e8d6a1260958b592038e916fa29685
saurabh4123 <saurabhdixit412@gmail.com>
2023-05-10 01:02:47 +0530

    jwt frontend done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import "./App.css";
+import axios from "axios";
 import Navbar from "./components/Navbar";
 import { EmployeeContextProvider } from "./contexts/employeeID";
 import AppRouter from "./router/AppRouter";
+import { baseUrl } from "./utils/constans";
+
+// Set the base URL for Axios
+axios.defaults.baseURL = baseUrl;
+
+// Add the JWT token to every request
+axios.interceptors.request.use(
+  (config) => {
+    const user=JSON.parse(localStorage.getItem("user"));
+    const token = user.token;
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 function App() {
   return (
     <>
-    <EmployeeContextProvider>
-      <Navbar />
-      <AppRouter />
+      <EmployeeContextProvider>
+        <Navbar />
+        <AppRouter />
       </EmployeeContextProvider>
     </>
   );
